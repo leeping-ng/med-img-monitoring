68073e94a5ef217ed7c17ba61c9baa597d93faab
leeping-ng <leeping_ng@hotmail.com>
2023-06-22 17:06:12 +0100

    added k-s test on confidence scores and ran experiment

--- src/predict.py
+++ src/predict.py
@@ -51,29 +51,14 @@ if __name__ == "__main__":
     pl.seed_everything(33, workers=True)
     trainer = pl.Trainer(enable_progress_bar=False)
 
-    # df = pd.DataFrame(
-    #     columns=[
-    #         "Transform",
-    #         "K-S cls0 stat",
-    #         "K-S cls0 pval",
-    #         "K-S cls1 stat",
-    #         "K-S cls1 pval",
-    #         "K-S signal",
-    #         "K-S shift",
-    #         "Conf orig",
-    #         "Conf shifted",
-    #         "Chi-Sq stat",
-    #         "Chi-Sq pval",
-    #         "Chi-Sq shift",
-    #     ]
-    # )
-
     df_tf = pd.DataFrame(
         columns=[
             "Transform",
-            "K-S signal",
-            "K-S pval",
-            "K-S acc",
+            "K-S SM signal",
+            "K-S SM pval",
+            "K-S SM acc",
+            "K-S conf signal",
+            "K-S conf acc",
             "Chi-Sq signal",
             "Chi-Sq pval",
             "Chi-Sq acc",
@@ -98,13 +83,14 @@ if __name__ == "__main__":
         # fix sequence of randomness so that sequence of images for each transform are the same
         random.seed(0)
 
-        ks_signals = []
-        ks_pvals = []
-        ks_detected_shifts = 0
+        ks_sm_signals = []
+        ks_sm_pvals = []
+        ks_sm_detected_shifts = 0
+        ks_conf_signals = []
+        ks_conf_detected_shifts = 0
         chisq_signals = []
         chisq_pvals = []
         chisq_detected_shifts = 0
-        chisq_errors = 0
 
         # loop over experiment runs with randomly shuffled images
         for run in range(NUM_RUNS):
@@ -125,12 +111,12 @@ if __name__ == "__main__":
             )
 
             # initialise empty data structures to be extended over batches
-            ks_original_softmaxes = {}
-            ks_shifted_softmaxes = {}
-            ks_result = {}
+            ks_sm_original_softmaxes = {}
+            ks_sm_shifted_softmaxes = {}
+            ks_sm_result = {}
             for c in range(num_classes):
-                ks_original_softmaxes[c] = []
-                ks_shifted_softmaxes[c] = []
+                ks_sm_original_softmaxes[c] = []
+                ks_sm_shifted_softmaxes[c] = []
             original_confs = []
             shifted_confs = []
             original_preds = []
@@ -149,82 +135,89 @@ if __name__ == "__main__":
                 original_softmax = original_batch["softmax"].numpy()
                 shifted_softmax = shifted_batch["softmax"].numpy()
 
-                # K-S test
+                # K-S taking in softmax
                 for c in range(num_classes):
-                    ks_original_softmaxes[c].extend(
+                    ks_sm_original_softmaxes[c].extend(
                         list(original_softmax[:, c].squeeze())
                     )
-                    ks_shifted_softmaxes[c].extend(
+                    ks_sm_shifted_softmaxes[c].extend(
                         list(shifted_softmax[:, c].squeeze())
                     )
 
-                # Confidence scores
-                original_confs.extend(list(np.amax(original_softmax, axis=1)))
-                shifted_confs.extend(list(np.amax(shifted_softmax, axis=1)))
+                # K-S taking in confidence scores
+                # find indexes of highest confidence score in original softmax
+                idx_max_confs = np.argmax(original_softmax, axis=1, keepdims=True)
+                original_confs.extend(
+                    list(
+                        np.take_along_axis(
+                            original_softmax, idx_max_confs, axis=1
+                        ).squeeze()
+                    )
+                )
+                # find confidence scores of shifted softmax that corresponds to idx_max_confs
+                shifted_confs.extend(
+                    list(
+                        np.take_along_axis(
+                            shifted_softmax, idx_max_confs, axis=1
+                        ).squeeze()
+                    )
+                )
 
                 # Chi-Squared test labels
                 original_preds.extend(list(original_batch["preds"].numpy()))
                 shifted_preds.extend(list(shifted_batch["preds"].numpy()))
 
-            # K-S test
-            ks_shift_detected = False
-            ks_signal = 0
+            # K-S taking in softmax
+            ks_sm_shift_detected = False
+            ks_sm_signal = 0
             for c in range(num_classes):
-                ks_result[c] = stats.ks_2samp(
-                    ks_original_softmaxes[c], ks_shifted_softmaxes[c]
+                ks_sm_result[c] = stats.ks_2samp(
+                    ks_sm_original_softmaxes[c], ks_sm_shifted_softmaxes[c]
                 )
                 # Reject null hypothesis if any p-value < Bonferroni corrected significance level
-                if ks_result[c].pvalue < configs["inference"]["alpha"] / num_classes:
-                    ks_shift_detected = True
-                ks_signal += ks_result[c].statistic
-            ks_signal /= num_classes
+                if ks_sm_result[c].pvalue < configs["inference"]["alpha"] / num_classes:
+                    ks_sm_shift_detected = True
+                ks_sm_signal += ks_sm_result[c].statistic
+            ks_sm_signal /= num_classes
 
-            # Confidence scores
-            original_avg_conf = sum(original_confs) / len(original_confs)
-            shifted_avg_conf = sum(shifted_confs) / len(shifted_confs)
+            ks_sm_signals.append(ks_sm_signal)
+            ks_sm_pvals.append((ks_sm_result[0].pvalue + ks_sm_result[1].pvalue) / 2)
+            if ks_sm_shift_detected:
+                ks_sm_detected_shifts += 1
+
+            # K-S taking in confidence scores
+            ks_conf_shift_detected = False
+            ks_conf_signal = 0
+            ks_conf_result = stats.ks_2samp(original_confs, shifted_confs)
+            if ks_conf_result.pvalue < configs["inference"]["alpha"]:
+                ks_conf_shift_detected = True
+            ks_conf_signal += ks_conf_result.statistic
+
+            ks_conf_signals.append(ks_conf_signal)
+            if ks_conf_shift_detected:
+                ks_conf_detected_shifts += 1
 
             # Chi-Squared test labels
             original_counts = np.bincount(original_preds, minlength=num_classes)
             shifted_counts = np.bincount(shifted_preds, minlength=num_classes)
-
             chisq_stat, chisq_p = stats.chisquare(shifted_counts, original_counts)
             if chisq_p < configs["inference"]["alpha"]:
                 chisq_shift_detected = True
             else:
                 chisq_shift_detected = False
 
-            # res = {
-            #     "Transform": tf_name,
-            #     "K-S cls0 stat": ks_result[0].statistic,
-            #     "K-S cls0 pval": ks_result[0].pvalue,
-            #     "K-S cls1 stat": ks_result[1].statistic,
-            #     "K-S cls1 pval": ks_result[1].pvalue,
-            #     "K-S signal": ks_signal,
-            #     "K-S shift": ks_shift_detected,
-            #     "Conf orig": original_avg_conf,
-            #     "Conf shifted": shifted_avg_conf,
-            #     "Chi-Sq stat": chisq_stat,
-            #     "Chi-Sq pval": chisq_p,
-            #     "Chi-Sq shift": chisq_shift_detected,
-            # }
-
-            ks_signals.append(ks_signal)
-            ks_pvals.append((ks_result[0].pvalue + ks_result[1].pvalue) / 2)
             chisq_signals.append(chisq_stat)
             chisq_pvals.append(chisq_p)
-
-            if ks_shift_detected:
-                ks_detected_shifts += 1
             if chisq_shift_detected:
                 chisq_detected_shifts += 1
 
-            # df = pd.concat([df, pd.DataFrame([res])], ignore_index=True)
-
         res_tf = {
             "Transform": tf_name,
-            "K-S signal": sum(ks_signals) / NUM_RUNS,
-            "K-S pval": sum(ks_pvals) / NUM_RUNS,
-            "K-S acc": ks_detected_shifts / NUM_RUNS,
+            "K-S SM signal": sum(ks_sm_signals) / NUM_RUNS,
+            "K-S SM pval": sum(ks_sm_pvals) / NUM_RUNS,
+            "K-S SM acc": ks_sm_detected_shifts / NUM_RUNS,
+            "K-S conf signal": sum(ks_conf_signals) / NUM_RUNS,
+            "K-S conf acc": ks_conf_detected_shifts / NUM_RUNS,
             "Chi-Sq signal": sum(chisq_signals) / NUM_RUNS,
             "Chi-Sq pval": sum(chisq_pvals) / NUM_RUNS,
             "Chi-Sq acc": chisq_detected_shifts / NUM_RUNS,
@@ -232,7 +225,6 @@ if __name__ == "__main__":
 
         df_tf = pd.concat([df_tf, pd.DataFrame([res_tf])], ignore_index=True)
 
-    # print(df)
     print(df_tf)
     os.makedirs(configs["inference"]["result_folder"], exist_ok=True)
     time_now = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
